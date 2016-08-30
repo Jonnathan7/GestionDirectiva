fc20187dc683ca9219e0a3b26ed99bc6edcd0ed5
Jonnathan7 <justindelabarra@gmail.com>
2016-08-30 15:08:04 -0500

    Entidad Plan de Area

---
+++ GDirectiva.Core.Entities/General/EN_PlanArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDirectiva.Core.Entities.General
+{
+    public class EN_PlanArea
+    {
+        public int Id_PlanArea { get; set; }
+        public string Nombre { get; set; }
+    }
+}
